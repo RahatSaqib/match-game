4a7bf44b544fffda2e3040fd582d094336e86928
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-11-10 21:47:28 +0600

    adding contracts for deployment

--- src/contracts/MatchGame.sol
+++ src/contracts/MatchGame.sol
@@ -1,8 +1,9 @@
 pragma solidity >=0.4.22 <0.9.0;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-contract MemoryToken is ERC721  {
+import "./ERC721Full.sol";
 
-    constructor() ERC721("Memory Token", "MEMORY") public {
+contract MatchGame is ERC721Full  {
+
+    constructor() ERC721Full("Match Game", "MEMORY") public {
     }
 
     function mint(address _to, string memory _tokenURI) public returns(bool) {
