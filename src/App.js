8ae0e96c09512c9892841a38a84e7e1596177e5f
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-11-12 15:28:17 +0600

    added resources and functionalities for game

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
   async componentWillMount() {
     await this.loadWeb3()
     await this.loadBlockchainData()
-    // this.setState({ cardArray: CARD_ARRAY.sort(() => 0.5 - Math.random()) })
+    this.setState({ gameArray: GAME_ARRAY.sort(() => 0.5 - Math.random()) })
   }
   async loadWeb3() {
     if (window.ethereum) {
@@ -107,6 +107,62 @@ class App extends Component {
       alert('Smart contract not deployed to detected network.')
     }
   }
+  chooseImage = (gameID) => {
+    gameID = gameID.toString()
+    if(this.state.gamesWon.includes(gameID)) {
+      return window.location.origin + '/images/white.png'
+    }
+    else if(this.state.cardsChosenId.includes(gameID)) {
+      return GAME_ARRAY[gameID].img
+    } else {
+      return window.location.origin + '/images/background.png'
+    }
+  }
+
+  flipCard = async (gameID) => {
+    let alreadyChosen = this.state.cardsChosen.length
+
+    this.setState({
+      cardsChosen: [...this.state.cardsChosen, this.state.gameArray[gameID].name],
+      cardsChosenId: [...this.state.cardsChosenId, gameID]
+    })
+
+    if (alreadyChosen === 1) {
+      setTimeout(this.checkForMatch, 100)
+    }
+  }
+
+
+  checkForMatch = async () => {
+    const optionOneId = this.state.cardsChosenId[0]
+    const optionTwoId = this.state.cardsChosenId[1]
+
+    if(optionOneId == optionTwoId) {
+      alert('You have clicked the same image!')
+    } else if (this.state.cardsChosen[0] === this.state.cardsChosen[1]) {
+      alert('You found a match')
+      this.state.token.methods.mint(
+        this.state.account,
+        window.location.origin + GAME_ARRAY[optionOneId].img.toString()
+      )
+      .send({ from: this.state.account })
+      .on('transactionHash', (hash) => {
+        this.setState({
+          gamesWon: [...this.state.gamesWon, optionOneId, optionTwoId],
+          tokenURIs: [...this.state.tokenURIs, GAME_ARRAY[optionOneId].img]
+        })
+      })
+    } else {
+      alert('Sorry, try again')
+    }
+    this.setState({
+      cardsChosen: [],
+      cardsChosenId: []
+    })
+    if (this.state.gamesWon.length === GAME_ARRAY.length) {
+      alert('Congratulations! You found them all!')
+    }
+  }
   constructor(props) {
     super(props)
     this.state = {
@@ -115,10 +171,10 @@ class App extends Component {
       token: null,
       totalSupply: 0,
       tokenURIs: [],
-      cardArray: [],
+      gameArray: [],
       cardsChosen: [],
       cardsChosenId: [],
-      cardsWon: []
+      gamesWon: []
     }
   }
 
@@ -127,7 +183,58 @@ class App extends Component {
     <div className="App">
       <Navbar account = {this.state.account}
               accountBalance ={this.state.accountBalance}/>
-    </div>
+      <div className="container-fluid mt-5">
+            <div className="row">
+              <main role="main" className="col-lg-12 d-flex text-center">
+                <div className="content mr-auto ml-auto">
+                  <h1 className="d-4">Start matching now!</h1>
+
+                  <div className="grid mb-4" >
+
+                    { this.state.gameArray.map((card, key) => {
+                      return(
+                        <img className ="gameImage"
+                          key={key}
+                          src={this.chooseImage(key)}
+                          data-id={key}
+                          onClick={(event) => {
+                            let gameID = event.target.getAttribute('data-id')
+                            if(!this.state.gamesWon.includes(gameID.toString())) {
+                              this.flipCard(gameID)
+                            }
+                          }}
+                        />
+                      )
+                    })}
+
+
+                  </div>
+
+                  <div>
+
+                    <h5>Tokens Collected:<span id="result">&nbsp;{this.state.tokenURIs.length}</span></h5>
+
+                    <div className="grid mb-4" >
+
+                      { this.state.tokenURIs.map((tokenURI, key) => {
+                        return(
+                          <img
+                            key={key}
+                            src={tokenURI}
+                          />
+                        )
+                      })}
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </main>
+            </div>
+      </div>
+      </div>
   );}
 }
 
